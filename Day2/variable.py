eb9b2af8a6c90cd603d83f36d29f1701edf17624
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-11 21:51:13 +0530

    Variable & Data type

---
+++ Day2/variable.py
@@ -0,0 +1,40 @@
+a=1
+b=True
+c="Harry"
+d=None
+print(a,b,c,d, sep="--")
+'''
+What is variable?
+variable is like a container who holds data,
+Creating a variable is like creating a placeholder in memory and assigning
+it some value. Ex- a,b,c,d,Ram,shyam 
+
+What is data type?
+Data type specifies the type of value a variable holds.
+
+Ex-
+
+Numeric data:- int, float, complex
+int: 3,-8,0
+float: 7.349,-9.0,0.0000001
+complex: 6 + 2i 
+
+Text data:- str
+str: "Hello!", "Bye"
+
+Boolean data:- Boolean data consist of values True or False
+
+Sequenced data:- list, tuple
+list:- A list is an ordered collection of data with elements seperated by comma and 
+enclosed within square brackets. List are mutable and can be modified after creation.
+list1=[8,2.3,[-4,5],["apple","banana"]]
+
+Tuple:- A tuple is an ordered collection of data with elements seperated by a comma and 
+enclosed within parentheses. Tuples are immutable and can not be modified after creation.
+tuple1=(("Parrot","Crow"),("Lion","Elephant"))
+
+Mapped data:- dict
+Dict:- A dictionary is a unordered collection of data containing a key:value pair.
+The key:value pairs are enclosed within curly brackets.
+dict1={"name":"Shivam","age":20,"canVote":True}
+'''
