127b9bd0c20be31dab7c6d0f17700089518d5eb4
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-11 21:09:55 +0530

    use of seprator

---
+++ Day2/seprator.py
@@ -0,0 +1,7 @@
+print("shivam",6,7, sep="~", end="009\n")
+print("Done!")
+print("Got it!")
+'''sep='seprator': Specify how to seperate the objects,
+if there is more than one. default is '' '''
+'''end='end' : Specify what to print at the end.
+Default is '\n' (line feed)'''
