ea86eca9578c00b1a8090ebcdffcd69d6ca14828
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-12 22:45:41 +0530

    Typecasting

---
+++ Day3/typecasting.py
@@ -0,0 +1,26 @@
+a="1"
+b="2"
+print(int(a) + int(b)) 
+
+''' 
+Typecasting in python:- The conversion of one data type into the 
+other data type is known as type casting in python or type conversion
+in python.
+
+python widely supports a wide variety of functions or methods like:
+int(),float(),str(),ord(),hex(),oct(),tuple(),set(),list(),dict(),
+etc for type casting in python.
+
+Types of type casting
+1. Explict conversion:- When a developer manually changes a 
+data type using built in functions like int() or str().
+
+2. Implicit conversion:-When the Python interpreter automatically 
+handles the data type change to prevent data loss, such as adding
+an integer to a float.'''
+
+# Implicit typecasting
+
+c=1.9
+d=8
+print(c+d)
