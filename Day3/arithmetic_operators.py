8cad874017be0bfec762778c39f5c913a2f18299
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-12 21:41:28 +0530

    Arithmetic Operators

---
+++ Day3/arithmetic_operators.py
@@ -0,0 +1,7 @@
+print(5+5) # Addition
+print(5-5) # Subtraction
+print(5*5) # Multiplication
+print(5**5) # Exponential
+print(5/5) # Division
+print(15%15) # Modulus
+print(15//15) # Floor Division
