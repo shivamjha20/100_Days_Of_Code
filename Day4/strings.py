aae0617946edf3e8260470d5d5a0a0e55e3c20b0
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-13 21:21:55 +0530

    Multiline Strings

--- Day4/strings.py
+++ Day4/strings.py
@@ -7,3 +7,11 @@ or double quotation mark is considered as string. A string is
 essentially a sequence or array of textual data. Strings are
 used when working with unicode characters.'''
 
+# Multiline Strings
+
+a='''
+In Python, multiline strings are created by wrapping text
+in triple quotes—either  single or  double—allowing you
+to span multiple lines without needing manual escape characters
+like \n.'''
+print(a)
