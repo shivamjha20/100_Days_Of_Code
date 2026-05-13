df9474db4710c5170025fe2fca00eaa3f3bcaf99
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-13 21:30:05 +0530

    Accessing characters of a string

--- Day4/strings.py
+++ Day4/strings.py
@@ -1,6 +1,7 @@
+print("Strings\n")
 name="shivam"
 friend="saif"
-print("Strings")
+print(name,"\n")
 '''
 Strings:- In python anything that you enclose between single
 or double quotation mark is considered as string. A string is
@@ -8,10 +9,15 @@ essentially a sequence or array of textual data. Strings are
 used when working with unicode characters.'''
 
 # Multiline Strings
-
+print("Multiline Strings")
 a='''
 In Python, multiline strings are created by wrapping text
 in triple quotes—either  single or  double—allowing you
 to span multiple lines without needing manual escape characters
 like \n.'''
 print(a)
+
+# Accessing characters of a string
+print("Accessing characters of a string")
+print(name[0])
+print(name[1])
