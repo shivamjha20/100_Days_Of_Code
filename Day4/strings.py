6194e2eb5ba84c519c1502e3174d0063e5f57659
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-13 21:12:52 +0530

    String

---
+++ Day4/strings.py
@@ -0,0 +1,9 @@
+name="shivam"
+friend="saif"
+print("Strings")
+'''
+Strings:- In python anything that you enclose between single
+or double quotation mark is considered as string. A string is
+essentially a sequence or array of textual data. Strings are
+used when working with unicode characters.'''
+
