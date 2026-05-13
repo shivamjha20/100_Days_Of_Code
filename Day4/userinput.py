8dc900dd9618dc59e1c2265444e1677df5d64d46
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-13 20:33:50 +0530

    User input

---
+++ Day4/userinput.py
@@ -0,0 +1,11 @@
+a = input("Enter your name: ")
+print("My name is ",a)
+'''
+In python we can take user input directly  by using input().
+
+But input function returns the value as string. Hence we have to
+typecast them whenever required to another datatype.
+Example:-
+variable=int(input())
+variable=float(input())
+'''
