755a385586ed6014f34eff84d0c6b6c0d679a5fa
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-14 22:52:14 +0530

    String as an array

--- Day5/string_operations.py
+++ Day5/string_operations.py
@@ -2,3 +2,10 @@
 fruit= "apple"
 len1=len(fruit)
 print("Apple is a ",len1," letter word")
+
+# String As an Array
+'''A string is essentially a sequence of characters also called
+ an array. Thus we can access the elements of this array'''
+name="shivam"
+print(name[:6])
+print(name[5])
