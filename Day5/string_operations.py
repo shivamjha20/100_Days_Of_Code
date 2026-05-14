3765025a04ca23091ee5fe640983b665b3fa94b9
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-14 22:59:02 +0530

    String Slicing

--- Day5/string_operations.py
+++ Day5/string_operations.py
@@ -9,3 +9,12 @@ print("Apple is a ",len1," letter word")
 name="shivam"
 print(name[:6])
 print(name[5])
+
+# String slicing
+''' The method of specifying the start and the end index to specify
+a part of a string is called slicing'''
+name="saif"
+print(name[:5]) # Slicing from start
+print(name[5:]) # Slicing till end
+print(name[2:5]) # Slicing in between
+print(name[-3:]) # Slicing using negative index
