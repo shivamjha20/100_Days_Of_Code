0ae0ddc554d5e1230da2284c399474b3c7ed0dec
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-15 05:17:09 +0530

    Upper() method

---
+++ Day6/string_methods.py
@@ -0,0 +1,7 @@
+# String methods
+'''Python provides a set of built in methods that we can use
+to ulter and modify the strings'''
+
+# upper():- The upper() methods a string to upper class.
+name="shivam"
+print(name.upper())
