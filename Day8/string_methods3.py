92e724b9eaa3bb77e0f243dd58716eddf0a94e1c
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-17 21:43:39 +0530

    Use of capitalize()

---
+++ Day8/string_methods3.py
@@ -0,0 +1,8 @@
+'''capitalize():- The capitalize() method turns only the first 
+character of the string to uppercase and the rest other character
+of the string are turned to lowercase. The string has no effect if 
+the first character is already uppercase.'''
+str="hello"
+print(str.capitalize())
+str2="hello World"
+print(str2.capitalize())
