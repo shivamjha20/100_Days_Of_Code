17264a038fa6621a73fd5d3f81f4ed3a561f054a
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-18 22:08:15 +0530

    Use of index()

--- Day9/string_methods4.py
+++ Day9/string_methods4.py
@@ -4,3 +4,11 @@ If given value is absent from the string then return -1.'''
 str1="He is a good teacher"
 print(str1.find("a"))
 print(str1.find("to"))
+
+'''index():- The index() method searches for the first occurrence
+of the given value and returns the index where it is present.
+If the given value is absent from the string then raise an
+exception'''
+str2="He is a good teacher"
+print(str2.index("a"))
+print(str2.index("to"))#Similar to find() but if value is absent it raises exception.
