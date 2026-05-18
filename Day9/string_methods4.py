918e25b72a4e13956d2b3e0703e1518750ad324d
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-18 22:36:50 +0530

    Use of isalnum()

--- Day9/string_methods4.py
+++ Day9/string_methods4.py
@@ -12,3 +12,11 @@ exception'''
 str2="He is a good teacher"
 print(str2.index("a"))
 #Similar to find() but if value is absent it raises exception.
+
+'''isalnum():- The isalnum() method returns true only if the string
+only consists of A-Z,a-z,0-9. If any other character or punctuations 
+are present , then it returns false.'''
+str2="Welcometohell"
+str3="Welcome to hell"
+print(str2.isalnum())
+print(str3.isalnum())
