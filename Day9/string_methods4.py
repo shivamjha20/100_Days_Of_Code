3257264dc0962249bc0ddd22ef0efba537e1a803
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-18 21:59:34 +0530

    Use of find()

---
+++ Day9/string_methods4.py
@@ -0,0 +1,6 @@
+'''find():- The find() method searches for the first occurence
+of the given value and return the index where it is present.
+If given value is absent from the string then return -1.'''
+str1="He is a good teacher"
+print(str1.find("a"))
+print(str1.find("to"))
