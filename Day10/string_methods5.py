cf042369aafe80c2c618b622c652a6b34d46425c
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-19 21:44:58 +0530

    Use of isalpha()

---
+++ Day10/string_methods5.py
@@ -0,0 +1,8 @@
+'''isalpha():- The isalnum() method returns true only if the 
+entire string only consists of A-Z,a-z. If any other 
+character or punctuations or numbers(0-9) are present , 
+then it returns false.'''
+str1="welcome"
+str2="welcome!!"
+print(str1.isalpha())
+print(str2.isalpha())
