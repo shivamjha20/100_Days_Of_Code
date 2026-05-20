3fdd984ef48cd8b878dbd4e8c6a438717760ff98
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-20 20:45:54 +0530

    Use of istitle()

---
+++ Day11/strings_methods6.py
@@ -0,0 +1,8 @@
+'''istitle():- The istitle() method returns True only if the 
+first letter of each word in the string is capitalized,
+ otherwise it returns False.'''
+str1 = "World Health Organization"
+print(str1.istitle())   # Output: True
+
+str2 = "To kill a Mocking bird"
+print(str2.istitle())   # Output: False
