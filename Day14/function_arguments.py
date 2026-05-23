2a06a2af5202b706800be7e8b72368c4601f5523
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-23 21:51:22 +0530

    Positional arguments

---
+++ Day14/function_arguments.py
@@ -0,0 +1,5 @@
+#Positional Arguments:-Passed in order, matched by position.
+def greet(name, age):
+    print(f"Hello {name}, you are {age} years old.")
+
+greet("Shivam", 24)   # Hello Shivam, you are 24 years old.
