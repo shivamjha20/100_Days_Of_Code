6f43de112074220b5f8efc1e691c5ea8c059aa38
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-23 21:58:39 +0530

    variable-Length arguments

--- Day14/function_arguments.py
+++ Day14/function_arguments.py
@@ -17,3 +17,10 @@ def greet(name="Guest"):
 
 greet("Shivam")   # Hello Shivam
 greet()           # Hello Guest
+
+'''Variable-Length Arguments (*args):-
+Accepts multiple positional arguments as a tuple.'''
+def add_numbers(*args):
+    print(sum(args))
+
+add_numbers(1, 2, 3, 4)   # 10
