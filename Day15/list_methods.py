1986acd7602992672a65e3e27fdd2dfc1654fc3f
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-24 21:35:31 +0530

    Use of insert()

--- Day15/list_methods.py
+++ Day15/list_methods.py
@@ -2,3 +2,7 @@
 fruits = ["apple", "banana"]
 fruits.append("cherry")
 print(fruits)   # ['apple', 'banana', 'cherry']
+
+#insert() → Adds an item at a specific position.
+fruits.insert(1, "mango")
+print(fruits)   # ['apple', 'mango', 'banana', 'cherry']
