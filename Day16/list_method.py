f623b8a60d0376135b7118e4eb0e804b933f979f
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-25 20:58:40 +0530

    Use of index()

--- Day16/list_method.py
+++ Day16/list_method.py
@@ -15,3 +15,7 @@ print(numbers)   # [1, 2, 3, 4]
 #reverse() → Reverses the list order.
 numbers.reverse()
 print(numbers)   # [4, 3, 2, 1]
+
+#index() → Returns the index of the first occurrence.
+nums = [10, 20, 30, 20]
+print(nums.index(20))   # 1
