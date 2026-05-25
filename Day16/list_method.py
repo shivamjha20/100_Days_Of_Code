ccd3b8e147c270344b39d1d2a0ed995a051685cf
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-25 20:56:04 +0530

    Use of sort()

--- Day16/list_method.py
+++ Day16/list_method.py
@@ -7,3 +7,7 @@ print(fruits)   # ['apple', 'mango']
 fruits.clear()
 print(fruits)   # []
 
+#sort() → Sorts the list in ascending order.
+numbers = [3, 1, 4, 2]
+numbers.sort()
+print(numbers)   # [1, 2, 3, 4]
