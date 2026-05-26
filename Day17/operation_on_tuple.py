099e8a4bf96ac283a9e5240656aa03430c661d4b
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-26 20:07:43 +0530

    Use of concatenation in tuple

--- Day17/operation_on_tuple.py
+++ Day17/operation_on_tuple.py
@@ -3,3 +3,9 @@ tup = (10, 20, 30, 40, 50)
 print(tup[0])       # 10
 print(tup[-1])      # 50
 print(tup[1:4])     # (20, 30, 40)
+
+#Concatenation:-Combine two tuples using +.
+tup1 = (1, 2, 3)
+tup2 = (4, 5)
+result = tup1 + tup2
+print(result)       # (1, 2, 3, 4, 5)
