117f09674fdc6aebc565bbe82c5e7db1e2d480b7
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-29 19:30:33 +0530

    What is set?

---
+++ Day19/set.py
@@ -0,0 +1,8 @@
+'''What is set?
+->A set is a collection of unique, unordered elements.
+
+->Defined using curly braces {} or the set() constructor.
+
+->Sets are useful when you want to store items without duplicates
+and perform mathematical set operations (union, intersection,
+difference).'''
