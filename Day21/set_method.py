df1b91a110ee4e464333611a1c8ec33ca688e904
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-31 21:30:34 +0530

    set method difference()

--- Day21/set_method.py
+++ Day21/set_method.py
@@ -2,3 +2,8 @@
 a = {1, 2, 3}
 b = {2, 3, 4}
 print(a.intersection(b))   # {2, 3}
+
+#difference() → Elements in one set but not the other.
+a = {1, 2, 3}
+b = {2, 3}
+print(a.difference(b))   # {1}
