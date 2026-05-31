86d434aa55dc611d00b40bd1a9455a234d9617d0
shivamjha20 <shivamkumar50290@gmail.com>
2026-05-31 21:32:24 +0530

    set method symmetric_difference()

--- Day21/set_method.py
+++ Day21/set_method.py
@@ -7,3 +7,9 @@ print(a.intersection(b))   # {2, 3}
 a = {1, 2, 3}
 b = {2, 3}
 print(a.difference(b))   # {1}
+
+#symmetric_difference() → Elements in either set but not both.
+a = {1, 2, 3}
+b = {3, 4}
+print(a.symmetric_difference(b))   # {1, 2, 4}
+
