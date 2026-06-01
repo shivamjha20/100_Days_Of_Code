5c70759a0c3871c26386fa56a0618a11f95fb629
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-01 21:05:32 +0530

    dictionary method values()

--- Day22/dictionary_methods.py
+++ Day22/dictionary_methods.py
@@ -1,3 +1,6 @@
 #keys() → Returns all keys.
 student = {"name": "Shivam", "age": 24}
 print(student.keys())   # dict_keys(['name', 'age'])
+
+#values() → Returns all values.
+print(student.values())   # dict_values(['Shivam', 24])
