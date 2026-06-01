40a38cd5ead0774b8fbb54df8f83189c43461693
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-01 21:07:33 +0530

    dictionary method get()

--- Day22/dictionary_methods.py
+++ Day22/dictionary_methods.py
@@ -8,3 +8,7 @@ print(student.values())   # dict_values(['Shivam', 24])
 #items() → Returns key-value pairs as tuples.
 print(student.items())   # dict_items([('name', 'Shivam'), ('age', 21)])
 
+#get() → Returns value for a key (safe, no error if missing).
+print(student.get("name"))   # Shivam
+print(student.get("city", "Not Found"))   # Not Found
+
