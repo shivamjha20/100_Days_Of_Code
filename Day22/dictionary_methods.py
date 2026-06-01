470c854f3f19d5054ebaaa3ee1bae27750b2a14d
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-01 21:06:29 +0530

    dictionary method items()

--- Day22/dictionary_methods.py
+++ Day22/dictionary_methods.py
@@ -4,3 +4,7 @@ print(student.keys())   # dict_keys(['name', 'age'])
 
 #values() → Returns all values.
 print(student.values())   # dict_values(['Shivam', 24])
+
+#items() → Returns key-value pairs as tuples.
+print(student.items())   # dict_items([('name', 'Shivam'), ('age', 21)])
+
