0e7f56bd4e0fc678fe1cf011f5b41d186bda247a
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-02 21:15:19 +0530

    dictionary method popitem()

--- Day23/dictionary_methods.py
+++ Day23/dictionary_methods.py
@@ -2,3 +2,7 @@
 student = {"name": "Shivam", "age": 22, "city":"Bihar"}
 print(student.pop("city"))   # Jharkhand
 print(student)   # {'name': 'Shivam', 'age': 22}
+
+#popitem() → Removes and returns the last inserted key-value pair.
+print(student.popitem())   # ('age', 22)
+print(student)   # {'name': 'Shivam'}
