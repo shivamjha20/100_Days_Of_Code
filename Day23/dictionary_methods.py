86836a619614edadf2ed2daa8b9f61d596d6458c
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-02 21:13:32 +0530

    dictionary method pop()

---
+++ Day23/dictionary_methods.py
@@ -0,0 +1,4 @@
+#pop() → Removes a key and returns its value.
+student = {"name": "Shivam", "age": 22, "city":"Bihar"}
+print(student.pop("city"))   # Jharkhand
+print(student)   # {'name': 'Shivam', 'age': 22}
