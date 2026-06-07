afac0c258162f7179f76474d8f8dd41d87a8f3e7
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-07 21:40:39 +0530

    Reading files

--- Day28/file_io.py
+++ Day28/file_io.py
@@ -13,3 +13,18 @@ with open("example.txt", "w") as f:
     f.write("Hello, world!\n")
     f.write("This is a second line.")
 #Using "with" ensures the file closes automatically.
+
+# Read entire file
+with open("example.txt", "r") as f:
+    content = f.read()
+    print(content)
+
+# Read line by line
+with open("example.txt", "r") as f:
+    for line in f:
+        print(line.strip())
+
+# Read into a list
+with open("example.txt", "r") as f:
+    lines = f.readlines()
+    print(lines)
