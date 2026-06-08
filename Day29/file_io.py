930c7a97dd98b7581356e37fbd71ac69ceada3ce
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-08 20:40:00 +0530

    File handling functions

--- Day29/file_io.py
+++ Day29/file_io.py
@@ -1,3 +1,9 @@
 #Appending to a File.
 with open("example.txt", "a") as f:
     f.write("\nThis line was appended.")
+
+#File handling functions
+import os
+print(os.path.exists("example.txt"))  # Check if file exists
+print(os.path.getsize("example.txt")) # File size in bytes
+    
