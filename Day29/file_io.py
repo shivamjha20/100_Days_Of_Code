0f3e690b9a443bb2a18962ec3738cc9dc6509b61
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-08 20:53:40 +0530

    Tell()

--- Day29/file_io.py
+++ Day29/file_io.py
@@ -6,4 +6,12 @@ with open("example.txt", "a") as f:
 import os
 print(os.path.exists("example.txt"))  # Check if file exists
 print(os.path.getsize("example.txt")) # File size in bytes
-    
+
+'''tell():-
+Returns the current position of the file pointer (in bytes).
+Useful for debugging or tracking where you are in the file.'''
+with open("example.txt", "r") as f:
+    print(f.tell())       # 0 (start of file)
+    f.read(5)
+    print(f.tell())       # 5 (after reading 5 characters)
+
