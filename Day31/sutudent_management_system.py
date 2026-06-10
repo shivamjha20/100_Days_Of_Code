1ff5f460830682a9af2cea312076c44adf773e68
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-10 21:30:35 +0530

    Student class inherits from person

--- Day31/sutudent_management_system.py
+++ Day31/sutudent_management_system.py
@@ -6,3 +6,14 @@ class Person:
     
     def show_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
+
+# Student class inherits from Person
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)   # reuse Person's constructor
+        self.student_id = student_id
+        self.courses = []
+    
+    def enroll(self, course):
+        self.courses.append(course)
+        print(f"{self.name} enrolled in {course}")
