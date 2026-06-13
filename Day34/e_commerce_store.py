5ae53045710aff9c60434d3620efc13cd8f5b61b
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-13 22:31:00 +0530

    Customer class

--- Day34/e_commerce_store.py
+++ Day34/e_commerce_store.py
@@ -2,3 +2,20 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.cart = []
+
+    def add_to_cart(self, product):
+        self.cart.append(product)
+        print(f"{self.name} added {product.name} to cart.")
+
+    def checkout(self):
+        if not self.cart:
+            print("Cart is empty!")
+            return
+        total = sum(p.price for p in self.cart)
+        print(f"{self.name}'s total bill: ₹{total}")
+        self.cart.clear()
