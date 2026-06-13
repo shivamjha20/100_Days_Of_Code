3996606ff641c940ab411d047bafc4c84b13b901
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-13 22:31:50 +0530

    Store class

--- Day34/e_commerce_store.py
+++ Day34/e_commerce_store.py
@@ -19,3 +19,19 @@ class Customer:
         total = sum(p.price for p in self.cart)
         print(f"{self.name}'s total bill: ₹{total}")
         self.cart.clear()
+
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def show_products(self):
+        if not self.products:
+            print("No products available.")
+            return
+        print(f"Products available in {self.name}:")
+        for i, p in enumerate(self.products, start=1):
+            print(f"{i}. {p.name} - ₹{p.price}")
