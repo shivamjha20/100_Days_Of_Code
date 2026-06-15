718bf0fe0349b66919b7c7b5175388a44245c513
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-15 21:28:41 +0530

    Teacher class

--- Day36/school_grading_system.py
+++ Day36/school_grading_system.py
@@ -21,3 +21,12 @@ class Student(Person):
             print("No grades yet.")
         for course, grade in self.grades.items():
             print(f"- {course}: {grade}")
+
+class Teacher(Person):
+    def __init__(self, name, subject):
+        super().__init__(name)
+        self.subject = subject
+
+    def assign_grade(self, student, course, grade):
+        student.grades[course.course_name] = grade
+        print(f"{self.name} assigned {grade} to {student.name} in {course.course_name}")
