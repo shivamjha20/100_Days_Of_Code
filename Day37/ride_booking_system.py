8cc10040308cfd6bb8753fffd48f4ec2e460ec08
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-16 21:49:48 +0530

    Driver class

--- Day37/ride_booking_system.py
+++ Day37/ride_booking_system.py
@@ -1,3 +1,13 @@
 class Person:
     def __init__(self, name):
         self.name = name
+
+class Driver(Person):
+    def __init__(self, name, vehicle, rate_per_km):
+        super().__init__(name)
+        self.vehicle = vehicle
+        self.rate_per_km = rate_per_km
+        self.available = True
+
+    def toggle_availability(self):
+        self.available = not self.available
