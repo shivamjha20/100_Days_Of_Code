2a794b3836eaa6cb68cd8422db925a308801398f
shivamjha20 <shivamkumar50290@gmail.com>
2026-06-18 22:54:02 +0530

    Inventory class

--- Day39/inventory_management_system.py
+++ Day39/inventory_management_system.py
@@ -7,3 +7,25 @@ class Product:
 
     def __str__(self):
         return f"{self.product_id} - {self.name} | ₹{self.price} | Stock: {self.quantity}"
+    
+class Inventory:
+    def __init__(self):
+        self.products = {}
+
+    def add_product(self, product):
+        self.products[product.product_id] = product
+        print(f"Product {product.name} added.")
+
+    def update_stock(self, product_id, quantity):
+        if product_id in self.products:
+            self.products[product_id].quantity += quantity
+            print(f"Stock updated for {self.products[product_id].name}.")
+        else:
+            print("Product not found.")
+
+    def show_inventory(self):
+        print("\n--- Inventory ---")
+        if not self.products:
+            print("No products available.")
+        for product in self.products.values():
+            print(product)
